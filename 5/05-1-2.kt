e335d505145337fc8dd2f72628be5dcb775e1f73
DoDi <4717jun57@naver.com>
2023-03-16 18:02:05 +0900

    2023/03/16 Update

---
+++ 5/05-1-2.kt
@@ -0,0 +1,9 @@
+package com.doyoung.andoridlab
+
+fun main() {
+    val some = {no1: Int, no2: Int ->
+        println("in lambda function")
+        no1 * no2
+    }
+    println("result : ${some(10, 20)}")
+}
