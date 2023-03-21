bdf447de2fe395ad3be32f11ac21384c232e42bb
DoDi <4717jun57@naver.com>
2023-03-21 22:34:42 +0900

    2023/03/21 Update

---
+++ SchoolStudy/ch6_1_visibility/app/src/main/java/com/example/ch6_1_visibility/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.ch6_1_visibility
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.ch6_1_visibility.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.visibleBtn.setOnClickListener {
+            binding.targetView.visibility = View.VISIBLE
+        }
+        binding.invisibleBtn.setOnClickListener{
+            binding.targetView.visibility = View.INVISIBLE
+        }
+
+    }
+}
