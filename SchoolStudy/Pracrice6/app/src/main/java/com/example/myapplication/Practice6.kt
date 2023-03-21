a571b765c3e6433e42d5d89bd60461a732d0b6ea
DoDi <4717jun57@naver.com>
2023-03-21 22:35:06 +0900

    2023/03/21 Update

--- SchoolStudy/Pracrice6/app/src/main/java/com/example/myapplication/Practice6.kt
+++ SchoolStudy/Pracrice6/app/src/main/java/com/example/myapplication/Practice6.kt
@@ -1,2 +1,13 @@
 package com.example.myapplication
 
+fun hotFun(function:(Int,Int) -> Boolean,arg1:Int, arg2:Int) : () -> String{
+    val result = if (function(arg1, arg2)) arg1
+                    else arg2
+    return {"Result: $result"}
+}
+
+fun main() {
+    println(hotFun({a,b ->a >b}, 10, 20)())
+    println(hotFun({a,b ->a > b},20, 10)())
+    println(hotFun({a,b -> a> b},20,20)())
+}
