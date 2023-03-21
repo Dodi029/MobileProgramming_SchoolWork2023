aaf84f2055f9b9ca61eaaed97a639095c06ef3a0
DoDi <4717jun57@naver.com>
2023-03-21 22:34:54 +0900

    2023/03/21 Update

--- SchoolStudy/KakaoTalk_Password_CheckView/ch6_view/src/main/java/com/example/ch6_view/MainActivity.kt
+++ SchoolStudy/KakaoTalk_Password_CheckView/ch6_view/src/main/java/com/example/ch6_view/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.ch6_view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.example.ch6_view.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.pass
     }
 }
