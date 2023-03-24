773007c41816e5f0d83b465783ef06c87feb63ee
DoDi <4717jun57@naver.com>
2023-03-24 23:14:38 +0900

    2023/03/24 Update

--- SchoolStudy/Lesson7/07FrameLayout/app/src/main/java/com/example/framelayout/MainActivity.kt
+++ SchoolStudy/Lesson7/07FrameLayout/app/src/main/java/com/example/framelayout/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.framelayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.framelayout.databinding.ActivityMainBinding
+import com.example.framelayout.databinding.ActivityMainBinding.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        val binding = inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.myButton.setOnClickListener {
+            binding.myButton.visibility = View.INVISIBLE
+            binding.myImageView.visibility = View.VISIBLE
+        }
+        binding.myImageView.setOnClickListener {
+            binding.myButton.visibility = View.VISIBLE
+            binding.myImageView.visibility = View.INVISIBLE
+        }
     }
 }
