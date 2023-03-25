51b291ad07a4a8f9d32e8fa9ad921e4e6cc80de5
DoDi <4717jun57@naver.com>
2023-03-25 23:27:49 +0900

    2023/03/25 Update

--- HomeStudy/03/Practice3-2-4_delay_lazy/app/src/main/java/com/example/practice03_lay/delay_lazy.kt
+++ HomeStudy/03/Practice3-2-4_delay_lazy/app/src/main/java/com/example/practice03_lay/delay_lazy.kt
@@ -1,2 +1,12 @@
 package com.example.practice03_lay
 
+val data4: Int by lazy {
+    println("inlazy......")
+    10
+}
+
+fun main() {
+    println("in main....")
+    println(data4 + 10)
+    println(data4 + 10)
+}
