a22923f91acd9f22000c825a81b04bc0264383f6
DoDi <4717jun57@naver.com>
2023-03-25 23:27:33 +0900

    2023/03/25 Update

--- HomeStudy/03/Practice03-2-5_BasicType/app/src/main/java/com/example/practice03_basictype/basic_type.kt
+++ HomeStudy/03/Practice03-2-5_BasicType/app/src/main/java/com/example/practice03_basictype/basic_type.kt
@@ -1,2 +1,21 @@
 package com.example.practice03_basictype
 
+fun main(){
+    val a1: Byte = 0b00001011
+
+    val a2: Int = 123
+    val a3: Short = 123
+    val a4: Long = 10L
+    val a5: Double = 10.0
+    val a6: Float = 10.0f
+
+    val a7: Boolean = true
+
+    val str1 = "Hello \n Wrld"
+    val str2 = """
+        Hello
+        World
+    """
+    println("str1 : $str1")
+    println("str2 : $str2")
+}
