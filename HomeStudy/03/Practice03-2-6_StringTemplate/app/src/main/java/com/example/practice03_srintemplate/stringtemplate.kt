658b9137bc7e436f85ec4386c9dd9dbbec049036
DoDi <4717jun57@naver.com>
2023-03-26 15:20:48 +0900

    2023/03/26 Update

--- HomeStudy/03/Practice03-2-6_StringTemplate/app/src/main/java/com/example/practice03_srintemplate/stringtemplate.kt
+++ HomeStudy/03/Practice03-2-6_StringTemplate/app/src/main/java/com/example/practice03_srintemplate/stringtemplate.kt
@@ -1,2 +1,35 @@
 package com.example.practice03_srintemplate
 
+fun main() {
+    fun sum(no: Int): Int {
+        var sum = 0
+        for (i in 1..no) {
+            sum += i
+        }
+        return sum
+    }
+
+    val name : String = "kkang"
+    println("name: $name, sum : ${sum(10)}, plus : ${10 + 20}")
+
+
+
+    val data: Nothing? = null
+
+
+
+    fun some1(): Nothing? {
+        return null
+    }
+    fun some2(): Nothing {
+        throw Exception()
+    }
+
+
+
+    // var data1: Int = 10
+    // data1 = null    // 오류
+
+    var data2: Int? = 10
+    data2 = null    // 성공
+}
--- HomeStudy/03/Practice03-2-7_ArrayFun/app/src/main/java/com/example/practice_arrayfun/arrayfun.kt
+++ HomeStudy/03/Practice03-2-7_ArrayFun/app/src/main/java/com/example/practice_arrayfun/arrayfun.kt
@@ -1,2 +1,33 @@
 package com.example.practice_arrayfun
 
+fun main() {
+    val data1: Array<Int> = Array(3, { 0 })
+    data1[0] = 10
+    data1[1] = 20
+    data1.set(2, 30)        // 배열에서 2번째 데이터를 30으로 설정
+
+    println(
+        """
+            array size : ${data1.size}
+            array data: ${data1[0]}, ${data1[1]}, ${data1.get(2)}
+        """
+    )
+
+
+
+    val data2: IntArray = IntArray(3, { 0 })
+    val data3: BooleanArray = BooleanArray(3, { false })
+
+
+    val data4 = intArrayOf(10, 20, 30)
+    val data5 = booleanArrayOf(true, false, true)
+
+
+    val data6 = arrayOf<Int>(10, 20, 30)
+    println(
+        """
+            array size : ${data1.size}
+            array data : ${data1[0]}, ${data1[1]}, ${data1.get(2)}
+        """
+    )
+}
