402e2c614b7cb6a0bf5c662744fae4633cf6bc04
DoDi <4717jun57@naver.com>
2023-03-30 14:25:51 +0900

    2023/03/30 Update

---
+++ SchoolStudy/Lesson8/08-2-2LongClick/ch8_event/src/main/java/com/example/ch8_event/Ch8_Event.kt
@@ -0,0 +1,11 @@
+package com.example.ch8_event
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ch8_Event : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ch8_event)
+    }
+}
