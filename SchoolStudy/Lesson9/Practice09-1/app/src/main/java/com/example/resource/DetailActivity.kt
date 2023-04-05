015999bbe42268033e69597af7ac10104539e66d
DoDi <4717jun57@naver.com>
2023-04-05 15:02:24 +0900

    2023/04/05 Update

--- SchoolStudy/Lesson9/Practice09-1/app/src/main/java/com/example/resource/DetailActivity.kt
+++ SchoolStudy/Lesson9/Practice09-1/app/src/main/java/com/example/resource/DetailActivity.kt
@@ -1,11 +1,20 @@
-package com.example.resource
+package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.resource.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "Detail Activity"
+
+        binding.result.text = intent.getStringExtra("result") + ":" +
+                intent.getIntExtra("data", 0)
+
+        binding.button.setOnClickListener{finish()}
     }
 }
--- SchoolStudy/Lesson9/Practice09-1/app/src/main/java/com/example/resource/MainActivity.kt
+++ SchoolStudy/Lesson9/Practice09-1/app/src/main/java/com/example/resource/MainActivity.kt
@@ -1,11 +1,20 @@
-package com.example.resource
+package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.resource.databinding.ActivityDetailBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "Main Activity"
+
+        binding.button.setOnClickListener{
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
