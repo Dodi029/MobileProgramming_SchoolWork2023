f88d5e00631717d692f4b92bee7344c7f54cdcd7
DoDi <4717jun57@naver.com>
2023-04-15 00:20:48 +0900

    2023/04/15 Update

--- SchoolStudy/PrepareTempExam/app/src/main/java/com/example/exam/DetailActivity.kt
+++ SchoolStudy/PrepareTempExam/app/src/main/java/com/example/exam/DetailActivity.kt
@@ -2,10 +2,14 @@ package com.example.exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exam.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_detail)
+        title = "Detail Activity"
     }
 }
--- SchoolStudy/PrepareTempExam/app/src/main/java/com/example/exam/MainActivity.kt
+++ SchoolStudy/PrepareTempExam/app/src/main/java/com/example/exam/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.exam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn.setOnClickListener {
+            val data = binding.btn.text.toString()
+            binding.te.text = data
+        }
     }
 }
