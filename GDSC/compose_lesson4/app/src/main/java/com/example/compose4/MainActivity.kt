167a45dfc9bd1f190036ede13829709086791089
DoDi <4717jun57@naver.com>
2023-05-18 00:36:20 +0900

    2023/05/18 Update

---
+++ GDSC/compose_lesson4/app/src/main/java/com/example/compose4/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.compose4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.compose4.ui.theme.Compose4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyColumn(
+                modifier = Modifier
+                    .background(color = Color.Green)
+                    .fillMaxWidth(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+
+                items(50){index ->
+                    Text("Footer")
+                }
+            }
+        }
+    }
+}
