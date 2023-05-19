7402f7a0d139b97478ffaeef8b553d69ab3ba505
DoDi <4717jun57@naver.com>
2023-05-20 03:05:02 +0900

    2023/05/20 Update

---
+++ SchoolStudy/Lesson15/jabscheduler/app/src/main/java/com/example/jobscheduler/MyJobService.kt
@@ -0,0 +1,12 @@
+package com.example.jobscheduler
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyJobService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
