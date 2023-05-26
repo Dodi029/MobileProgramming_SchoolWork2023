4f630f0e4454d9ddaa66a90e57ea4df5cca7e5ab
DoDi <4717jun57@naver.com>
2023-05-26 10:15:15 +0900

    2023/05/26 Updata

--- GDSC/compose_lesson6/app/src/main/java/com/example/compose6/MainActivity.kt
+++ GDSC/compose_lesson6/app/src/main/java/com/example/compose6/MainActivity.kt
@@ -7,28 +7,50 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.compose6.ui.theme.Compose6Theme
-
+import kotlinx.coroutines.launch
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Column(
-               modifier = Modifier.fillMaxSize(),
-               verticalArrangement = Arrangement.Center,
-               horizontalAlignment = Alignment.CenterHorizontally,
-           ) {
-               TextField(value = "",
-                   onValueChange = {},
-               )
-               Button(onClick = {}) {
-                   Text("클릭!!")
-               }
-           }
+            val (text, setValue) = remember {
+                mutableStateOf("")
+            }
+
+            val scaffoldState = rememberScaffoldState()
+            val scope = rememberCoroutineScope()
+            val keyboardController = LocalSoftwareKeyboardController.current
+
+            Scaffold(
+                scaffoldState = scaffoldState,
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    TextField(
+                        value = text,
+                        onValueChange = setValue
+                    )
+                    Button(onClick = {
+                        keyboardController?.hide()  // null 값일 수도 있기 때문에 ? 작성
+                        scope.launch{
+                            scaffoldState.snackbarHostState.showSnackbar("Hello $text")
+                        }
+                    }) {
+                        Text("클릭!!")
+                    }
+                }
+            }
+
         }
     }
 }
