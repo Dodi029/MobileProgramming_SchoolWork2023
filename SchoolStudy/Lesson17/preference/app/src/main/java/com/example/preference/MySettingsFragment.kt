f25e059d9682f2c291b4b424f4c34e239b94e7d2
DoDi <4717jun57@naver.com>
2023-05-31 00:42:43 +0900

    2023/05/31 Update

---
+++ SchoolStudy/Lesson17/preference/app/src/main/java/com/example/preference/MySettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.preference
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class MySettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+}
