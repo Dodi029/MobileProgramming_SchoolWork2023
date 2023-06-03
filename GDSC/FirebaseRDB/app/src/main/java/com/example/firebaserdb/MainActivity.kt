f590c145a780e74e90688cc88a3f7a70c5d0ede9
DoDi <4717jun57@naver.com>
2023-06-04 00:04:00 +0900

    2023/06/04 Update

--- GDSC/FirebaseRDB/app/src/main/java/com/example/firebaserdb/MainActivity.kt
+++ GDSC/FirebaseRDB/app/src/main/java/com/example/firebaserdb/MainActivity.kt
@@ -3,70 +3,105 @@ package com.example.firebaserdb
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.firebaserdb.ui.theme.FirebaseRDBTheme
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.gson.annotations.SerializedName
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            FirebaseRDBTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+data class Message(val text: String, val userId: String)
+
+@Composable
+fun ChatApp() {
+    val messages = remember { mutableStateListOf<Message>() }
+    val newMessageText = remember { mutableStateOf("") }
+
+    val messagesRef = FirebaseDatabase.getInstance().getReference("messages")
+
+    val valueEventListener = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            messages.clear()
+            for (messageSnapshot in snapshot.children) {
+                val message = messageSnapshot.getValue(Message::class.java)
+                message?.let {
+                    messages.add(it)
                 }
             }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            // 오류 처리
+        }
+    }
+
+    DisposableEffect(Unit) {
+        messagesRef.addValueEventListener(valueEventListener)
+        onDispose {
+            messagesRef.removeEventListener(valueEventListener)
+        }
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        // 채팅 메시지 표시
+        MessageList(messages)
+
+        // 메시지 입력란과 전송 버튼
+        MessageInput(newMessageText.value, onMessageSent = {
+            val newMessageRef = messagesRef.push()
+            val newMessage = Message(text = it, userId = "user1")
+            newMessageRef.setValue(newMessage)
+            newMessageText.value = ""
+        })
+    }
+}
+
+@Composable
+fun MessageList(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            Text(message.text)
         }
     }
 }
 
-//@Composable
-//fun Greeting(name: String) {
-//    Text(text = "Hello $name!")
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    FirebaseRDBTheme {
-//        Greeting("Android")
-//    }
-//}
+@Composable
+fun MessageInput(text: String, onMessageSent: (String) -> Unit) {
+    val context = LocalContext.current
 
-data class Nickname(
-    @SerializedName("id")
-    val id : Int,
-    @SerializedName("nickname")
-    val nickname : String
-)
-// 응답받을 데이터는 Boolean 타입
-
-object ApiObject {
-    private const val BASE_URL = (
-            //주소
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = { onMessageSent(it) },
+            modifier = Modifier.weight(1f),
+            label = { Text("메시지 입력") }
+        )
+        IconButton(
+            onClick = { onMessageSent(text) },
+            modifier = Modifier.padding(start = 8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = "Send"
             )
-
-    fun getApiClient(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        }
     }
-}
-
-interface AccessInterface {
-    @GET("/user/check")
 }
