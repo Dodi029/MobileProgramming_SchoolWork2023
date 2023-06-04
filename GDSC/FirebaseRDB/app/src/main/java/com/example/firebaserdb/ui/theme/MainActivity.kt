fbd7cf37565ba98164501ac095b60f9e12a45d3b
DoDi <4717jun57@naver.com>
2023-06-04 22:38:25 +0900

    2023/06/04 Update

--- GDSC/FirebaseRDB/app/src/main/java/com/example/firebaserdb/ui/theme/MainActivity.kt
+++ GDSC/FirebaseRDB/app/src/main/java/com/example/firebaserdb/ui/theme/MainActivity.kt
@@ -2,10 +2,111 @@ package com.example.firebaserdb.ui.theme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.firebaserdb.Message
+import com.example.firebaserdb.R
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+data class Message(val text: String, val userId: String)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}
+
+@Composable
+fun ChatApp() {
+    val messages = remember { mutableStateListOf<Message>() }
+    val newMessageText = remember { mutableStateOf("") }
+
+    val messagesRef = FirebaseDatabase.getInstance().getReference("messages")
+
+    val valueEventListener = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            messages.clear()
+            for (messageSnapshot in snapshot.children) {
+                val message = messageSnapshot.getValue(Message::class.java)
+                message?.let {
+                    messages.add(it)
+                }
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            // 오류 처리
+        }
+    }
+
+    DisposableEffect(Unit) {
+        messagesRef.addValueEventListener(valueEventListener)
+        onDispose {
+            messagesRef.removeEventListener(valueEventListener)
+        }
+    }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        // 채팅 메시지 표시
+        MessageList(messages)
+
+        // 메시지 입력란과 전송 버튼
+        MessageInput(newMessageText.value, onMessageSent = {
+            val newMessageRef = messagesRef.push()
+            val newMessage = Message(text = it, userId = "user1")
+            newMessageRef.setValue(newMessage)
+            newMessageText.value = ""
+        })
+    }
+}
+
+@Composable
+fun MessageList(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            Text(message.text)
+        }
+    }
+}
+
+@Composable
+fun MessageInput(text: String, onMessageSent: (String) -> Unit) {
+    val context = LocalContext.current
+
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = { onMessageSent(it) },
+            modifier = Modifier.weight(1f),
+            label = { Text("메시지 입력") }
+        )
+        IconButton(
+            onClick = { onMessageSent(text) },
+            modifier = Modifier.padding(start = 8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = "Send"
+            )
+        }
     }
 }
