e89b689a015dcfefe89d3589462244d63feda4bc
DoDi <4717jun57@naver.com>
2023-06-09 12:41:59 +0900

    2023/06/09 Update

--- SchoolStudy/Lesson10/notification/app/src/main/java/com/example/notification/MainActivity2.kt
+++ SchoolStudy/Lesson10/notification/app/src/main/java/com/example/notification/MainActivity2.kt
@@ -1,11 +1,25 @@
 package com.example.notification
 
+import android.app.NotificationManager
+import android.app.RemoteInput
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.notification.databinding.ActivityMain2Binding
+import com.example.notification.databinding.ActivityMainBinding
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "알림 결과"
+
+        val replyTxt = RemoteInput.getResultsFromIntent(intent)
+            ?.getCharSequence("key_text_reply")
+
+        binding.result.text = replyTxt
+
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(11)
     }
 }
