99df99f81f69c95f83719f507aab8512ea6efe0a
DoDi <4717jun57@naver.com>
2023-07-15 11:20:31 +1000

    2023/07/15 Update

--- SchoolStudy/Lesson11/recyclerview_gridlayout/app/src/main/java/com/example/recyclerviewgridlayout/MainActivity.kt
+++ SchoolStudy/Lesson11/recyclerview_gridlayout/app/src/main/java/com/example/recyclerviewgridlayout/MainActivity.kt
@@ -1,11 +1,61 @@
 package com.example.recyclerviewgridlayout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+// import com.example.recyclerview.databinding.ActivityMainBinding
+// import com.example.recyclerview.databinding.ItemMainBinding
+import com.example.recyclerviewgridlayout.databinding.ActivityMainBinding
+import com.example.recyclerviewgridlayout.databinding.ItemMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val datas = mutableListOf<String>()
+        var current = 1
+        for (i in 1..5){
+            datas.add("Item $i")
+            current = i
+        }
+
+        //binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        val layoutManager = GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false)
+        binding.recyclerView.layoutManager = layoutManager
+        val adapter = MyAdapter(datas)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        binding.addBtn.setOnClickListener {
+            datas.add("Item ${++current}")
+            adapter.notifyDataSetChanged()
+        }
+    }
+}
+
+class MyViewHolder(val binding: ItemMainBinding): RecyclerView.ViewHolder(binding.root)
+
+class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyViewHolder(ItemMainBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun getItemCount(): Int {
+        return datas.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+        binding.itemData.text = datas[position]
+        binding.itemData.setOnClickListener{
+            Log.d("kkang", "Item Click : $position")
+        }
     }
 }
