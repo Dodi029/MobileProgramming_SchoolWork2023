235f5516ff6830ec1c23d62989a5513582ba75b3
DoDi <4717jun57@naver.com>
2023-08-15 21:00:05 +1000

    2023/08/15 Update

---
+++ SchoolStudy/Lesson12/AppBar/app/src/main/java/com/example/appbar/MainActivity2.kt
@@ -0,0 +1,47 @@
+package com.example.appbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appbar.databinding.ActivityMain2Binding
+import com.example.appbar.databinding.ListItemBinding
+
+class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title="RecyclerView"
+
+        val datas = mutableListOf<String>()
+        for (i in 1..50){
+            datas.add("Item $i")
+        }
+
+        binding.listView.layoutManager = LinearLayoutManager(this)
+        binding.listView.adapter = MyAdapter(datas)
+        binding.listView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+    }
+}
+
+class MyViewHolder(val binding: ListItemBinding): RecyclerView.ViewHolder(binding.root)
+
+class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyViewHolder(ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+
+        binding.itemData.text = datas[position]
+    }
+
+    override fun getItemCount(): Int = datas.size
+}
