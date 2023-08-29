9a586489c2a2c955562ba4b27590a96ddcdd4c11
DoDi <4717jun57@naver.com>
2023-08-30 00:01:22 +0900

    2023/08/30 Update

--- SchoolStudy/Lesson12/AppBar_CollapsingToolbar/app/src/main/java/com/example/appbarcollapsing/MainActivity.kt
+++ SchoolStudy/Lesson12/AppBar_CollapsingToolbar/app/src/main/java/com/example/appbarcollapsing/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.appbarcollapsing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.example.appbarcollapsing.MainActivity2
+import com.example.appbarcollapsing.MainActivity3
+import com.example.appbarcollapsing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        title="App Bar"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu?.add(0, 0, 0, "RecycleView")?.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        menu?.add(0, 1, 0, "CollapsingToolbar")?.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            0 -> { startActivity(Intent(this, MainActivity2::class.java))}
+            1 -> { startActivity(Intent(this, MainActivity3::class.java))}
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
