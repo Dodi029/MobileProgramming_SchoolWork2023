0bdd76a02ac6f2b0440f4e122c739853d1b87d2e
DoDi <4717jun57@naver.com>
2023-08-31 21:22:24 +0900

    2023/08/31 Update

--- SchoolStudy/Lesson12/AppBar_CollapsingToolbar/app/src/main/java/com/example/appbarcollapsing/MainActivity2.kt
+++ SchoolStudy/Lesson12/AppBar_CollapsingToolbar/app/src/main/java/com/example/appbarcollapsing/MainActivity2.kt
@@ -2,10 +2,46 @@ package com.example.appbarcollapsing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appbarcollapsing.databinding.ActivityMain2Binding
+import com.example.appbarcollapsing.databinding.ListItemBinding
 
 class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title="RecyclerView"
+
+        val datas = mutableListOf<String>()
+        for (i in 1..50){
+            datas.add("Item $i")
+        }
+
+        binding.listView.layoutManager = LinearLayoutManager(this)
+        binding.listView.adapter = MyAdapter(datas)
+        binding.listView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
     }
+}
+
+class MyViewHolder(val binding: ListItemBinding): RecyclerView.ViewHolder(binding.root)
+
+class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyViewHolder(ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+
+        binding.itemData.text = datas[position]
+    }
+
+    override fun getItemCount(): Int = datas.size
 }
