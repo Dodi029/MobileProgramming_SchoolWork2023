bdae629c73e1253be0c19529935d2ab8bb5edb6f
DoDi <4717jun57@naver.com>
2023-09-16 21:05:40 +0900

    2023/09/16 Update

--- SchoolStudy/Lesson12/tab/app/src/main/java/com/example/tab/MainActivity3.kt
+++ SchoolStudy/Lesson12/tab/app/src/main/java/com/example/tab/MainActivity3.kt
@@ -1,11 +1,46 @@
 package com.example.tab
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tab.databinding.ActivityMain3Binding
+import com.example.tab.databinding.ItemPagerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        val binding = ActivityMain3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "뷰 페이저 연동"
+
+        val datas = listOf("Item 1", "Item 2", "Item 3")
+        binding.viewpager.adapter = MyPagerAdapter(datas)
+        TabLayoutMediator(binding.tabs, binding.viewpager) {tab, position ->
+            tab.text = "Tab${position +1}"
+        }.attach()
     }
+}
+
+class MyPagerViewHolder(val binding: ItemPagerBinding): RecyclerView.ViewHolder(binding.root)
+
+class MyPagerAdapter(val datas: List<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        MyPagerViewHolder(ItemPagerBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyPagerViewHolder).binding
+
+        binding.itemPagerTextView.text = datas[position]
+        when (position % 3) {
+            0 -> binding.itemPagerTextView.setBackgroundColor(Color.RED)
+            1 -> binding.itemPagerTextView.setBackgroundColor(Color.BLUE)
+            2 -> binding.itemPagerTextView.setBackgroundColor(Color.GREEN)
+        }
+    }
+    override fun getItemCount(): Int = datas.size
+
 }
