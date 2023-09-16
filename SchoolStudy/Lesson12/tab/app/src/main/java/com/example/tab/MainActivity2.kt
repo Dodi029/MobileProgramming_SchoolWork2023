1c5c55819b56a7f600b2f541543da66934e57628
DoDi <4717jun57@naver.com>
2023-09-16 21:05:36 +0900

    2023/09/16 Update

--- SchoolStudy/Lesson12/tab/app/src/main/java/com/example/tab/MainActivity2.kt
+++ SchoolStudy/Lesson12/tab/app/src/main/java/com/example/tab/MainActivity2.kt
@@ -2,10 +2,24 @@ package com.example.tab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.tab.databinding.ActivityMain2Binding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        val binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        title = "Tab Layout(XML)"
+
+        binding.tabs.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                Toast.makeText(applicationContext, tab?.text, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
     }
 }
