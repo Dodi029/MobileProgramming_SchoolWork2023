9387a73d2cde9f9be8e7576c9b0123ece19bffe4
DoDi <4717jun57@naver.com>
2023-10-17 13:27:11 +0900

    2023/10/17 Update

--- SchoolStudy/lesson14/broadcast/app/src/main/java/com/example/broadcastreceiver/MyReceiver.kt
+++ SchoolStudy/lesson14/broadcast/app/src/main/java/com/example/broadcastreceiver/MyReceiver.kt
@@ -1,4 +1,12 @@
 package com.example.broadcastreceiver
 
-class MyReceiver {
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class MyReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Toast.makeText(context, "정적 MyReceiver", Toast.LENGTH_SHORT).show()
+    }
 }
