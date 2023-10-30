9c3689e848fa81f9644b7e4b1fad56279a3cdfef
DoDi <4717jun57@naver.com>
2023-10-30 13:05:00 +0900

    2023/10/30 Update

--- SchoolStudy/Lesson15/bindingservice/app/src/main/java/com/example/bindingservice/MyService.kt
+++ SchoolStudy/Lesson15/bindingservice/app/src/main/java/com/example/bindingservice/MyService.kt
@@ -1,12 +1,27 @@
 package com.example.bindingservice
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
-import android.os.IBinder
+import android.os.*
+import android.widget.Toast
+
 
 class MyService : Service() {
+    lateinit var messenger: Messenger
+    internal class IncomingHandler(val context: Context) : Handler(Looper.getMainLooper()){
+        override fun handleMessage(msg: Message) {
+            when (msg.what) {
+                10 -> Toast.makeText(context.applicationContext, "${msg.obj}", Toast.LENGTH_SHORT).show()
+                20 -> Toast.makeText(context.applicationContext, "${(msg.obj as Bundle).getString("data")}", Toast.LENGTH_SHORT).show()
+                else -> super.handleMessage(msg)
+            }
+        }
+    }
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    override fun onBind(intent: Intent) : IBinder {
+        Toast.makeText(applicationContext, "MyService-onBind", Toast.LENGTH_SHORT).show()
+        messenger = Messenger(IncomingHandler(this))
+        return messenger.binder
     }
 }
