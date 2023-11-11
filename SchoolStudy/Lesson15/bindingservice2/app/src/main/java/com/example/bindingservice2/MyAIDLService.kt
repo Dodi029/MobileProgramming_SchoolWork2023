def82451e3780a34415f12be75912d04b7b65c43
DoDi <4717jun57@naver.com>
2023-11-12 00:46:30 +0900

    2023/11/12 Update

--- SchoolStudy/Lesson15/bindingservice2/app/src/main/java/com/example/bindingservice2/MyAIDLService.kt
+++ SchoolStudy/Lesson15/bindingservice2/app/src/main/java/com/example/bindingservice2/MyAIDLService.kt
@@ -3,10 +3,21 @@ package com.example.bindingservice2
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.widget.Toast
 
 class MyAIDLService : Service() {
-
+    var name: String = ""
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return object: MyAidlInterface.Stub() {
+            override fun funA(data: String) {}
+            override fun funB(): Int {
+                return 10
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        Toast.makeText(applicationContext, "MyAIDLService-onDestroy", Toast.LENGTH_SHORT).show()
+        super.onDestroy()
     }
 }
