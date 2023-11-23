ef824387d53f9afd13f6ac2e9d26a274fe8cb4b5
DoDi <4717jun57@naver.com>
2023-11-23 15:35:04 +0900

    2023/11/23 Update

--- SchoolStudy/Lesson15/jabscheduler/app/src/main/java/com/example/jobscheduler/MainActivity.kt
+++ SchoolStudy/Lesson15/jabscheduler/app/src/main/java/com/example/jobscheduler/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.example.jobscheduler
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.core.content.getSystemService
+import com.example.jobscheduler.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    val JOBID = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val receiver = object: BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                binding.resultText.text = intent?.getStringExtra("RESULT_TITLE") + ": +" +
+                        intent?.getIntExtra("RESULT",0).toString()
+            }
+        }
+
+        val filter = IntentFilter("SEND_RESULT")
+        registerReceiver(receiver, filter)
+
+        val jobScheduler: JobScheduler? = getSystemService<JobScheduler>()
+        lateinit var jobInfo: JobInfo
+        binding.startJobService.setOnClickListener {
+            binding.resultText.text = "Result ?"
+            JobInfo.Builder(JOBID, ComponentName(this, MyJobService::class.java)).run{
+                val extra = PersistableBundle()
+                extra.putString("TITLE", "Hello Job")
+                setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                setExtras(extra)
+                jobInfo = build()
+                jobScheduler?.schedule(jobInfo)
+            }
+        }
+        binding.cancelJobService.setOnClickListener {
+            jobScheduler?.cancel(JOBID)
+        }
     }
 }
