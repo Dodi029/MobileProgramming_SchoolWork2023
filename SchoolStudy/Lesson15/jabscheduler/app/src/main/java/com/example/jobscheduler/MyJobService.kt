e7d7b8f239303ac6ab2480468079e2caff448a15
DoDi <4717jun57@naver.com>
2023-11-23 15:35:08 +0900

    2023/11/23 Update

--- SchoolStudy/Lesson15/jabscheduler/app/src/main/java/com/example/jobscheduler/MyJobService.kt
+++ SchoolStudy/Lesson15/jabscheduler/app/src/main/java/com/example/jobscheduler/MyJobService.kt
@@ -1,12 +1,50 @@
 package com.example.jobscheduler
 
 import android.app.Service
+import android.app.job.JobParameters
+import android.app.job.JobService
 import android.content.Intent
 import android.os.IBinder
+import android.os.SystemClock
+import android.widget.Toast
 
-class MyJobService : Service() {
+class MyJobService : JobService() {
 
-    override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+    var bStop = false
+    override fun onCreate() {
+        super.onCreate()
+        Toast.makeText(applicationContext, "MyJobService-onCreate", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(applicationContext, "MyJobService-onDestroy", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStartJob(params: JobParameters?): Boolean {
+        Toast.makeText(applicationContext, "MyJobService-onStartJob", Toast.LENGTH_SHORT).show()
+
+        Thread(Runnable {
+            val intent = Intent("SEND_RESULT")
+            intent.putExtra("RESULT_TITLE", params!!.extras.getString("TITLE"))
+
+            for (i in 1..20){
+                SystemClock.sleep(1000)
+
+                if(bStop) break
+                intent.putExtra("RESULT", i)
+                sendBroadcast(intent)
+            }
+
+            jobFinished(params, false)
+        }).start()
+
+        return true
+    }
+
+    override fun onStopJob(params: JobParameters?): Boolean {
+        Toast.makeText(applicationContext, "MyJobService-onStopJob", Toast.LENGTH_SHORT).show()
+        bStop = true
+        return false
     }
 }
