58c96224790217679a0ed0eda046ac30d94002c0
DoDi <4717jun57@naver.com>
2023-11-26 16:33:52 +0900

    2023/11/26 Update

--- SchoolStudy/Lesson15/service/app/src/main/java/com/example/service/MyService.kt
+++ SchoolStudy/Lesson15/service/app/src/main/java/com/example/service/MyService.kt
@@ -2,11 +2,35 @@ package com.example.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import android.widget.Toast
 
 class MyService : Service() {
+    class MyBinder : Binder() {
+        fun funA(arg: Int) {}
+        fun funB(arg: Int): Int {
+            return arg * arg
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Toast.makeText(applicationContext, "MyService-onCreate", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Toast.makeText(applicationContext, "MyService-onStartCommand", Toast.LENGTH_SHORT).show()
+        return super.onStartCommand(intent, flags, startId)
+    }
 
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        Toast.makeText(applicationContext, "MyService-onBind", Toast.LENGTH_SHORT).show()
+        return MyBinder()
+    }
+
+    override fun onDestroy() {
+        Toast.makeText(applicationContext, "MyService-onDestroy", Toast.LENGTH_SHORT).show()
+        super.onDestroy()
     }
 }
