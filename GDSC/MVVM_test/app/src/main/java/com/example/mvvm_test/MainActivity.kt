bebeeaae043e68a2083c4ac162d97c83ecbfa4c4
DoDi <4717jun57@naver.com>
2023-12-11 22:03:13 +0900

    2023/12/11 Update

---
+++ GDSC/MVVM_test/app/src/main/java/com/example/mvvm_test/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.mvvm_test
+
+import android.R
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import com.example.mvvm_test.databinding.ActivityMainBinding
+import java.util.logging.Logger
+
+
+class MainActivity : AppCompatActivity() {
+    var binding // 상속 ViewDataBinding
+            : ActivityMainBinding? = null
+    var viewModel: ViewModel? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("Main_onCrete() 실행")
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        ) // Activity Content's View - Layout 연결 & 반환 : ViewDataBinding을 상속하는 제네릭 타입
+        viewModel = ViewModel(Database.getInstance())
+        binding.setViewModel(viewModel)
+        binding.okBtnview.setOnClickListener(object : OnClickListener() {
+            fun onClick(view: View?) {
+                Logger.d("버튼 클릭")
+                viewModel.getUser()
+            }
+        })
+    }
+}
